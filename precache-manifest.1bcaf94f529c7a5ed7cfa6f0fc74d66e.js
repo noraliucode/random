beaa87a70a1b76b7758cb2e19a88661a1a04fd7c
Nora Liu <tzuyuanliu0830@gmail.com>
2019-10-01 15:35:38 +0800

    Updates

---
+++ precache-manifest.1bcaf94f529c7a5ed7cfa6f0fc74d66e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab47f1acfe0974e6e58f39272fe22119",
+    "url": "/random/index.html"
+  },
+  {
+    "revision": "083487d6eabacded0774",
+    "url": "/random/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c55e17d8402c594d8ff5",
+    "url": "/random/static/js/2.885db4fe.chunk.js"
+  },
+  {
+    "revision": "083487d6eabacded0774",
+    "url": "/random/static/js/main.e3edd076.chunk.js"
+  },
+  {
+    "revision": "890d24a48e20eee9e1ba",
+    "url": "/random/static/js/runtime-main.159b21fc.js"
+  }
+]);
